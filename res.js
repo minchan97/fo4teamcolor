90c39ae5d90ec2fa0564c32a89078f64a5f7c2d7
minchan97 <86109410+minchan97@users.noreply.github.com>
2021-10-20 00:56:28 +0900

    Add files via upload

---
+++ res.js
@@ -0,0 +1,12 @@
+// let a = '김철수'
+
+// let regex = new RegExp('\^'+ a + ' +');
+
+// console.log(regex.test('김철수 +3'));
+
+
+
+
+let a = ['김밥','떡볶이']
+
+console.log(typeof(a))
--- server.js
+++ server.js
@@ -25,10 +25,38 @@ app.get('/', function(req, res) {
 
 
 app.post('/result', function(req, res) {
-  res.sendFile(__dirname + '/views/result.html');
+  let abilityArray = req.body.ability;
+  if (typeof(abilityArray) == 'string') {
+    abilityArray = [];
+    abilityArray.push(req.body.ability);
+  }
   db.collection('teamcolor').find().toArray(function(err, result) {
+    let resultArray = [];
+    let total = 0;
     for (i=0;i<result.length;i++) {
-
+      let cnt = 0;
+      let teamAbility = [result[i].item1, result[i].item2, result[i].item3];
+      for (stat of abilityArray) {
+        let regex = new RegExp('\^'+ stat + ' +');
+        for (item of teamAbility) {
+          if (regex.test(item)) {
+            cnt ++;
+          }
+        }
+      }
+      if (abilityArray.length == cnt) {
+        resultArray.push(result[i]);
+        total ++;
+      }
+      cnt = 0;
+    }
+    if (!total) {
+      res.sendFile(__dirname + '/views/error.html');
+    } else {
+      res.render('list.ejs', { posts : resultArray });
     }
   });
 }); 
+
+
+
